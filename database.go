f2e5e474d3259c56b99b3e3689b2f9eb6daca53f
Jeff Mataya <jeff@jeffmataya.com>
2018-06-18 12:34:59 -0400

    Implement basic migrations

--- database.go
+++ database.go
@@ -3,15 +3,21 @@ package kin
 import (
 	"database/sql"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 // Database is an interface for interacting with the database. It abstracts
 // away managing connection pools and various other low-level bits.
 type Database interface {
+	// Migrate runs a series of database migrations.
+	Migrate(folderPath string) error
+
 	// Query generates a new query to be executed at a later time.
 	Query(stmt string, params ...interface{}) *Query
 
-	// StartTransaction
+	// StartTransaction initiates a database transaction object.
 	StartTransaction() (Transaction, error)
 }
 
@@ -26,6 +32,53 @@ func New(db *sql.DB) (Database, error) {
 
 type database struct {
 	db *sql.DB
+}
+
+func fileSuffix(fileName string) string {
+	parts := strings.Split(fileName, ".")
+	return parts[len(parts)-1]
+}
+
+func (d *database) Migrate(folderPath string) error {
+	fmt.Println("Starting database migrations...")
+	fmt.Println("")
+
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		return fmt.Errorf("unable to read migrations: %v", err)
+	}
+
+	sqlFiles := []string{}
+	for _, file := range files {
+		if !file.IsDir() && fileSuffix(file.Name()) == "sql" {
+			sqlFiles = append(sqlFiles, file.Name())
+		}
+	}
+
+	if len(sqlFiles) == 0 {
+		return fmt.Errorf("no migrations found in folder '%s'", folderPath)
+	}
+
+	for _, sqlFile := range sqlFiles {
+		fmt.Printf("-------- Running %s...", sqlFile)
+
+		file, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", folderPath, sqlFile))
+		if err != nil {
+			fmt.Printf("FAILED\n")
+			fmt.Println("-------- Rolling back changes.")
+			return fmt.Errorf("Error executing %s: %v", sqlFile, err)
+		}
+
+		_, err = d.db.Exec(string(file))
+		if err != nil {
+			fmt.Printf("FAILED\n")
+			fmt.Println("-------- Rolling back changes.")
+			return fmt.Errorf("Error executing %s: %v", sqlFile, err)
+		}
+
+		fmt.Printf("COMPLETED\n")
+	}
+	return nil
 }
 
 func (d *database) Query(stmt string, params ...interface{}) *Query {
---
+++ database_test.go
@@ -0,0 +1,119 @@
+package kin
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func setupMigrationDir(path string) error {
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func cleanupMigrationDir(path string) error {
+	return os.RemoveAll(path)
+}
+
+func createFile(path, name, contents string) error {
+	file, err := os.Create(fmt.Sprintf("%s/%s", path, name))
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	if _, err := file.WriteString(fmt.Sprintf("%s\n", contents)); err != nil {
+		return err
+	}
+
+	file.Sync()
+	return nil
+}
+
+func TestMigrate(t *testing.T) {
+	migrationPath := "./sql"
+
+	cleanupMigrationDir(migrationPath)
+	if err := setupMigrationDir(migrationPath); err != nil {
+		t.Errorf("setupMigrationDir = %v", err)
+		return
+	}
+
+	createFoo := "create table foo (id serial primary key);"
+	createBar := `
+  	create table bar (
+  		id serial primary key,
+  		foo_id int not null,
+  		foreign key (foo_id) references foo(id) on update restrict on delete restrict
+  	);
+  `
+	if err := createFile(migrationPath, "1__create_foo.sql", createFoo); err != nil {
+		t.Errorf("createFile = %v", err)
+		cleanupMigrationDir(migrationPath)
+		return
+	}
+
+	if err := createFile(migrationPath, "2__create_bar.sql", createBar); err != nil {
+		t.Errorf("createFile = %v", err)
+		cleanupMigrationDir(migrationPath)
+		return
+	}
+
+	connStr := "user=postgres dbname=kin_test sslmode=disable"
+	db, _ := sql.Open("postgres", connStr)
+	dbConn, _ := New(db)
+
+	if err := dbConn.Migrate(migrationPath); err != nil {
+		t.Errorf("dbConn.Migrate(%s) = %v, want nil", migrationPath, err)
+		cleanupMigrationDir(migrationPath)
+	}
+
+	cleanupMigrationDir(migrationPath)
+}
+
+func TestMigrateRerun(t *testing.T) {
+	migrationPath := "./sql"
+
+	cleanupMigrationDir(migrationPath)
+	if err := setupMigrationDir(migrationPath); err != nil {
+		t.Errorf("setupMigrationDir = %v", err)
+		return
+	}
+
+	createBaz := "create table baz (id serial primary key);"
+	if err := createFile(migrationPath, "1__create_baz.sql", createBaz); err != nil {
+		t.Errorf("createFile = %v", err)
+		cleanupMigrationDir(migrationPath)
+		return
+	}
+
+	connStr := "user=postgres dbname=kin_test sslmode=disable"
+	db, _ := sql.Open("postgres", connStr)
+	dbConn, _ := New(db)
+
+	if err := dbConn.Migrate(migrationPath); err != nil {
+		t.Errorf("dbConn.Migrate(%s) = %v, want nil", migrationPath, err)
+		cleanupMigrationDir(migrationPath)
+		return
+	}
+
+	want := `Error executing 1__create_baz.sql: pq: relation "baz" already exists`
+	err := dbConn.Migrate(migrationPath)
+	if err == nil {
+		t.Errorf("dbConn.Migrate(%s) = nil, want %s", migrationPath, want)
+		cleanupMigrationDir(migrationPath)
+		return
+	}
+
+	if err.Error() != want {
+		t.Errorf("dbConn.Migrate(%s) = %s, want %s", migrationPath, err.Error(), want)
+	}
+
+	cleanupMigrationDir(migrationPath)
+}
